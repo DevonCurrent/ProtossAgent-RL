9bd860d9756c2382a8c65f69d14e53dd5e25f6e9
Devon Current <dacurrent@bsu.edu>
2018-07-17 23:20:32 -0400

    select units, build units and structures, and defeat easy ai

--- ProtossAgent.py
+++ ProtossAgent.py
@@ -7,13 +7,95 @@ import random
 class ProtossAgent(base_agent.BaseAgent):
 	def __init__(self):
 		super(ProtossAgent, self).__init__()
+		
+		self.attack_coordinates = None
+
+	def unit_type_is_selected(self, obs, unit_type):
+		if (len(obs.observation.single_select) > 0 and
+		obs.observation.single_select[0].unit_type == unit_type):
+				return True
+
+		if (len(obs.observation.multi_select) > 0 and
+		obs.observation.multi_select[0].unit_type == unit_type):
+			return True
+
+		return False
+
+	def get_units_by_type(self, obs, unit_type):
+		return [unit for unit in obs.observation.feature_units if unit.unit_type == unit_type]
+
+	def can_do(self, obs, action):
+		return action in obs.observation.available_actions
+
+	def select_available_probe(self, obs):
+		probes = self.get_units_by_type(obs, units.Protoss.Probe)
+		if len(probes) > 0:
+			probe = random.choice(probes)
+			return actions.FUNCTIONS.select_point("select_all_type", (probe.x,
+			probe.y))
+
+	def step(self, obs):
+		super(ProtossAgent, self).step(obs)
+		if obs.first():
+			agent_y, agent_x = (obs.observation.feature_minimap.player_relative == 
+			features.PlayerRelative.SELF).nonzero()
+
+			agent_xmean = agent_x.mean()
+			agent_ymean = agent_y.mean()
+			
+			if agent_xmean <= 31 and agent_ymean <= 31:
+				self.attack_coordinates = (49, 49)
+			else:
+				self.attack_coordinates = (12, 16)
+
+		zealots = self.get_units_by_type(obs, units.Protoss.Zealot)
+		if len(zealots) >= 12:
+			if self.unit_type_is_selected(obs, units.Protoss.Zealot):
+				if self.can_do(obs, actions.FUNCTIONS.Attack_minimap.id):
+					return actions.FUNCTIONS.Attack_minimap("now",
+					self.attack_coordinates)
+
+			if self.can_do(obs, actions.FUNCTIONS.select_army.id):
+				return actions.FUNCTIONS.select_army("select")
+
+		pylon = self.get_units_by_type(obs, units.Protoss.Pylon)
+		free_supply = (obs.observation.player.food_cap - obs.observation.player.food_used)
+		if free_supply < 15:
+			if self.unit_type_is_selected(obs, units.Protoss.Probe):
+				if self.can_do(obs, actions.FUNCTIONS.Build_Pylon_screen.id):
+					x = random.randint(0, 83)
+					y = random.randint(0, 83)
+					
+					return actions.FUNCTIONS.Build_Pylon_screen("now", (x, y))
+
+			self.select_available_probe(obs)
 
 
-def step(self, obs):
-	super(ProtossAgent, self).step(obs)
-	
-	
-	return actions.FUNCTIONS.no_op()
+		gateways = self.get_units_by_type(obs, units.Protoss.Gateway)
+		if len(gateways) <= 2:
+			if self.unit_type_is_selected(obs, units.Protoss.Probe):
+				if self.can_do(obs, actions.FUNCTIONS.Build_Gateway_screen.id):
+					x = random.randint(0, 83)
+					y = random.randint(0, 83)
+
+					return actions.FUNCTIONS.Build_Gateway_screen("now", (x, y))
+
+			probes = self.get_units_by_type(obs, units.Protoss.Probe)
+			if len(probes) > 0:
+				probe = random.choice(probes)
+
+				return actions.FUNCTIONS.select_point("select_all_type", (probe.x,
+				probe.y))
+
+		if self.can_do(obs, actions.FUNCTIONS.Train_Zealot_quick.id):
+			return actions.FUNCTIONS.Train_Zealot_quick("now")
+
+		if len(gateways) > 0:
+			gateway = random.choice(gateways)
+			return actions.FUNCTIONS.select_point("select_all_type", (gateway.x, gateway.y))
+
+		return actions.FUNCTIONS.no_op()
+
 
 def main(unused_argv):
 	agent = ProtossAgent()
